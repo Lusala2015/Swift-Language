509ca8792fbb8a48ff4dc61bb1db792bb10f1cfb
Lusala2015 <lusalamark17@gmail.com>
2020-01-17 12:23:26 +0300

    first commit

---
+++ 1.0variables.swift
@@ -0,0 +1,10 @@
+//Swift infers the type of variable, so you don't always have to declare variable type:
+var name = "Mark"
+print(name)
+var stockPrice = 200
+stockPrice = 50
+print(stockPrice)
+ 
+ //keep track of data that you need but you don't expect to change   
+let lastName = "Lusala"
+print (lastName)
